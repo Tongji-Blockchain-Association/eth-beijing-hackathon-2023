5c226e5cacfb6f7f0c234f6f712d48f2f757020f
LeeJAJA <jiajie@mit.edu>
2023-04-08 09:37:21 +0800

    feat(extension): Added comment box

--- extension/src/components/EvidenceBox.js
+++ extension/src/components/EvidenceBox.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./EvidenceBox.css";
 
 const EvidenceBox = ({}) => {
-  return <div className=""></div>;
+  return (
+    <textarea
+      className="evidence-frame evidence-text"
+      defaultValue="Type in your EVIDENCE (optional)"
+      warp="hard"
+    ></textarea>
+  );
 };
 
 export default EvidenceBox;
